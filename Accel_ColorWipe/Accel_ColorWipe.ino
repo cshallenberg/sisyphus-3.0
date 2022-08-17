9b8914defbff3b633797115996b4976a01b51dc9
aldo-aguilar <aldoaguilar2022@u.northwestern.edu>
2022-08-16 21:34:26 -0500

    Added function for checking GPIO voltage

--- Accel_ColorWipe/Accel_ColorWipe.ino
+++ Accel_ColorWipe/Accel_ColorWipe.ino
@@ -1,43 +1,22 @@
-
 #include <SPI.h>
 #include <ADXL362.h>
 
-#include <OctoWS2811.h>
-
-int16_t temp;
 int16_t XValue, YValue, ZValue, Temperature;
 
 ADXL362 xl;
 
-const int ledsPerStrip = 120; 
-
-DMAMEM int displayMemory[ledsPerStrip*6];
-int drawingMemory[ledsPerStrip*6];
-
-uint8_t  frame = 0;    // Frame count, results displayed every 256 frames
-
-const int config = WS2811_GRB | WS2811_800kHz;
-
-OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config);
+#define WIN_STATE_PIN 0
 
 void setup() {
-  SPI.setMOSI(26); SPI.setMISO(1); SPI.setSCK(27); //change to second set of SPI pins
+//  SPI.setMOSI(26); SPI.setMISO(1); SPI.setSCK(27); //change to second set of SPI pins
   Serial.begin(9600);
   xl.begin(0);                   // Setup SPI protocol, issue device soft reset Chip Select pin 0
   xl.beginMeasure();              // Switch ADXL362 to measure mode  
   
   Serial.println("Start Demo: Simple Read");
-  
-  leds.begin();
-  leds.show();
-}
 
-#define RED    0xFF0000
-#define GREEN  0x00FF00
-#define BLUE   0x0000FF
-#define YELLOW 0xFFFF00
-#define PINK   0xFF1088
-#define ORANGE 0xE05800
+  pinMode(WIN_STATE_PIN, INPUT_PULLUP);
+}
 
 void loop() {
   xl.readXYZTData(XValue, YValue, ZValue, Temperature);  
@@ -50,25 +29,11 @@ void loop() {
   Serial.print("\tTEMPERATURE=");
   Serial.println(Temperature);   
   delay(100);                // Arbitrary delay to make serial monitor easier to observe
-  
-  int microsec = 2000000 / leds.numPixels();  // change them all in 2 seconds
-
-  // uncomment for voltage controlled speed
-  // millisec = analogRead(A9) / 40;
- 
-  colorWipe(RED, microsec);
-  colorWipe(GREEN, microsec);
-  colorWipe(BLUE, microsec);
-  colorWipe(YELLOW, microsec);
-  colorWipe(PINK, microsec);
-  colorWipe(ORANGE, microsec);
+  check_win_state(); 
 }
 
-void colorWipe(int color, int wait)
-{
-  for (int i=0; i < leds.numPixels(); i++) {
-    leds.setPixel(i, color);
-    leds.show();
-    delayMicroseconds(wait);
+void check_win_state(){
+  if (digitalRead(WIN_STATE_PIN)) {
+    Serial.print("win state entered");
   }
 }
